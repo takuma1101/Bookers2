f8eba0df7e868f09554d27a6973fcd6a4d458825
takuma1101 <mtnr.baseball0821@gmail.com>
2022-06-06 06:01:15 +0000

    Bookers2 kadai

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,43 @@
+class UsersController < ApplicationController
+
+  def show
+     @user = User.find(params[:id])
+     @users = current_user
+     @newbook = Book.new
+     @book = Book.where(user_id: @user.id)
+    @books = Book.all
+  end
+
+  def index
+     @user = current_user
+    @users = User.all
+    @book = Book.new
+    @books = Book.all
+  end
+
+  def edit
+    @user = User.find(params[:id])
+     if @user == current_user
+        render "edit"
+     else
+      redirect_to user_path(current_user)
+     end
+  end
+
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      flash[:notice]="You have updated user successfully."
+      redirect_to user_path(current_user)
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:profile_image, :name, :introduction)
+  end
+end
